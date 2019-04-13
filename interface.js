7bd3161ad1ace1f8d3e5d9a5761ee0dd197c6a47
Hui An Yang <hecatie36@hotmail.com>
2019-04-13 22:23:47 +0100

    initial interface working

---
+++ interface.js
@@ -0,0 +1,10 @@
+const loadPage = ()=>{
+    let productList = document.getElementById("productList")
+    let cartList = document.getElementById("cartList")
+    let totalBefore = document.getElementById("totalBefore")
+    let voucherIntput = document.getElementById("voucherInput")
+    let voucherButton = document.getElementById("voucherButton")
+    let totalAfter = document.getElementById("totalAfter")
+    productList.innerText = 'hiiii'
+}
+document.onload = loadPage()
