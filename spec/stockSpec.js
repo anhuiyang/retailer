e240441e24d4d6206d907089ee79ff7cecdbfa10
Hui An Yang <hecatie36@hotmail.com>
2019-04-13 22:59:56 +0100

    stock class initial stock test failed

---
+++ spec/stockSpec.js
@@ -0,0 +1,9 @@
+describe(`Stock`, ()=>{
+    let stock
+    beforeEach(()=>{
+        stock = new stock(initialStock)
+    })
+    it(`will initialize with available stock`, ()=>{
+        
+    })
+})
