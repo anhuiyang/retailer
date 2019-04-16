43c360f1cd0c48ab7383b0a2cd4cd980b32ffd9b
Hui An Yang <hecatie36@hotmail.com>
2019-04-16 14:57:24 +0100

    class cart func decrease test failed

--- spec/cartSpec.js
+++ spec/cartSpec.js
@@ -1,16 +1,34 @@
 describe(`Cart`, ()=>{
     let cart, cartData = [{itemId: `01`, quantity: 0}]
-    let stringName= `Almond Toe Court Shoes`
-    let stringQuantity = `Quantity: 1`
 
     it(`can increase in quantity`, ()=>{
         cart = new Cart(cartData)
         cart.increase(`01`)
         expect(cart.all[0].quantity).toBe(1)
-
     })
+})
+
+describe(`Cart`, ()=>{
+    let cart, cartData = [{itemId: `01`, quantity: 0}]
+    let stringName= `Almond Toe Court Shoes`
+    let stringQuantity = `Quantity: 1` 
     it(`can show cart item in array of strings`, ()=>{
+        cart = new Cart(cartData)
+        cart.increase(`01`)
         expect(cart.stringCart()[0]).toContain(stringName)
         expect(cart.stringCart()[0]).toContain(stringQuantity)
+    })
+})
+
+describe(`Cart`, ()=>{
+    let cart, cartData = [{itemId: `01`, quantity: 0}]
+    let stringName= `Almond Toe Court Shoes`
+    let stringQuantity = `Quantity: 1` 
+    it(`can show cart item in array of strings`, ()=>{
+        cart = new Cart(cartData)
+        cart.increase(`01`)
+        cart.decrease(`01`)
+        expect(cart.stringCart()[0]).not.toContain(stringName)
+        expect(cart.stringCart()[0]).not.toContain(stringQuantity)
     })
 })
--- spec/stockSpec.js
+++ spec/stockSpec.js
@@ -1,23 +1,30 @@
 describe(`Stock`, ()=>{
     let stock, stockData = [{itemId: `01`, quantity: 10}]
     let stringName= `Almond Toe Court Shoes`
-
+beforeEach(()=>{
+    stock = new Stock(stockData)
+})
     it(`will initialize with available stock`, ()=>{
-        stock = new Stock(stockData)
         expect(stock.all.length).toBe(1)
     })
     it(`can show stocks in array of strings`, ()=>{
-        stock = new Stock(stockData)
         expect(stock.stringStock()[0]).toContain(stringName)
     })
     it(`can decrease a stock`, ()=>{
-        stock = new Stock(stockData)
         stock.decrease(`01`)
         expect(stock.all[0].quantity).toBe(9)
     })
-    it(`can increase a stock`, ()=>{
+
+})
+
+describe(`Stock`, ()=>{
+    let stock, stockData = [{itemId: `01`, quantity: 10}]
+    let stringName= `Almond Toe Court Shoes`
+    beforeEach(()=>{
         stock = new Stock(stockData)
+    })
+    it(`can increase a stock`, ()=>{
         stock.increase(`01`)
-        expect(stock.all[0].quantity).toBe(10)
+        expect(stock.all[0].quantity).toBe(11)
     })
 })
