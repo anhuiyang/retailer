488348afd9d6df0a7b82352ab8f595286996ce05
Hui An Yang <hecatie36@hotmail.com>
2019-04-16 15:20:56 +0100

    class total func increase test passed

--- spec/totalSpec.js
+++ spec/totalSpec.js
@@ -7,4 +7,9 @@ describe(`Total`, ()=>{
     it(`will initialize with total 0`, ()=>{
         expect(total.before).toBe(0)
     })
+
+    it(`will increase amount and change before`, ()=>{
+        total.increase(`01`)
+        expect(total.before).toBe(99.00)
+    })
 })
--- src/total.js
+++ src/total.js
@@ -2,4 +2,8 @@ class Total{
     constructor(){
         this.before = 0
     }
+    increase=(itemId, product = Product)=>{
+        let eachProduct = new product(itemId)
+        this.before += eachProduct.price
+    }
 }
